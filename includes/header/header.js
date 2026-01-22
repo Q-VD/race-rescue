2645c061dad1e11c66b567af3c8a4105a1ec0b64
Q-VD <qvandommelen55@gmail.com>
2026-01-22 17:43:07 +0100

    Update header.js

--- includes/header/header.js
+++ includes/header/header.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/race-rescue/incluse/header/header.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("header-placeholder").innerHTML = data;
 
+    // titel uit body halen
+    const title = document.body.dataset.title;
+    const titleElement = document.getElementById("page-title");
+    if (title && titleElement) {
+      titleElement.textContent = title;
+    }
+
+    // logo uit body halen
+    const logoSrc = document.body.dataset.logo;
+    const logoElement = document.getElementById("site-logo");
+    if (logoSrc && logoElement) {
+      logoElement.src = logoSrc;
+    }
+  });
+});
